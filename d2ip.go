c2a70736a0f4ca9c2388d225cac0b03d4d196643
Urle Sistiana <55231606+urlesistiana@users.noreply.github.com>
2022-12-08 18:13:04 +0800

    small improvements

--- d2ip.go
+++ d2ip.go
@@ -18,3 +18,113 @@
  */
 
 package main
+
+import (
+	"github.com/miekg/dns"
+	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/zap"
+	"net/netip"
+	"strings"
+)
+
+type d2ip struct {
+	domains      map[string]struct{}
+	queryCounter prometheus.Counter
+	errCounter   prometheus.Counter
+}
+
+func (d *d2ip) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
+	resp := d.resp(w, r)
+	_ = w.WriteMsg(resp)
+}
+
+func (d *d2ip) resp(w dns.ResponseWriter, q *dns.Msg) *dns.Msg {
+	d.queryCounter.Inc()
+
+	// Reject invalid queries.
+	if len(q.Question) != 1 || q.Question[0].Qtype != dns.ClassINET {
+		logger.Warn("invalid query", zap.Stringer("from", w.RemoteAddr()))
+		d.errCounter.Inc()
+		return reject(q, dns.RcodeRefused)
+	}
+
+	question := q.Question[0]
+	// Parse ip addr from prefix.
+	s, suffixOk := trimFqdn(question.Name, d.domains)
+	if !suffixOk {
+		logger.Warn("invalid domain", zap.String("qname", question.Name), zap.Stringer("from", w.RemoteAddr()))
+		d.errCounter.Inc()
+		return reject(q, dns.RcodeRefused)
+	}
+
+	if strings.ContainsRune(s, '-') {
+		s = strings.ReplaceAll(s, "-", ":")
+	}
+	addr, err := netip.ParseAddr(s)
+	if err != nil {
+		logger.Warn("invalid ip", zap.String("qname", question.Name), zap.Stringer("from", w.RemoteAddr()))
+		d.errCounter.Inc()
+		return reject(q, dns.RcodeNameError)
+	}
+
+	logger.Info(
+		"incoming query",
+		zap.String("qname", question.Name),
+		zap.Uint16("qtype", question.Qtype),
+		zap.Stringer("ip", addr),
+		zap.Stringer("from", w.RemoteAddr()),
+	)
+
+	r := new(dns.Msg)
+	r.SetReply(q)
+	r.Authoritative = true
+	switch question.Qtype {
+	case dns.TypeA:
+		if !addr.Is4() {
+			break
+		}
+		r.Answer = append(r.Answer, &dns.A{
+			Hdr: dns.RR_Header{
+				Name:   question.Name,
+				Rrtype: dns.TypeA,
+				Class:  dns.ClassINET,
+				Ttl:    3600 * 24,
+			},
+			A: addr.AsSlice(),
+		})
+	case dns.TypeAAAA:
+		if !addr.Is6() {
+			break
+		}
+		r.Answer = append(r.Answer, &dns.AAAA{
+			Hdr: dns.RR_Header{
+				Name:   question.Name,
+				Rrtype: dns.TypeAAAA,
+				Class:  dns.ClassINET,
+				Ttl:    3600 * 24,
+			},
+			AAAA: addr.AsSlice(),
+		})
+	}
+	return r
+}
+
+func trimFqdn(fqdn string, domains map[string]struct{}) (string, bool) {
+	var suffixOk bool
+	for off, end := 0, false; !end; off, end = dns.NextLabel(fqdn, off) {
+		suffix := fqdn[off:]
+		if _, suffixOk = domains[suffix]; suffixOk {
+			if off > 0 {
+				return fqdn[:off-1], true
+			}
+			return "", true
+		}
+	}
+	return "", false
+}
+
+func reject(q *dns.Msg, rcode int) *dns.Msg {
+	r := new(dns.Msg)
+	r.SetRcode(q, rcode)
+	return r
+}
--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 	"go.uber.org/zap"
 	"net"
 	"net/http"
-	"net/netip"
 	"os"
 	"os/signal"
 	"strings"
+	"sync/atomic"
 	"syscall"
 )
 
-const version = "dev"
+var version = "dev"
 
 var (
 	domains     = flag.String("d", "", "root domain(s), split multiple domains using ','")
@@ -54,16 +54,21 @@ func main() {
 	reg := prometheus.NewRegistry()
 	reg.MustRegister(h.queryCounter, h.errCounter)
 
+	var closed atomic.Bool
+
 	// Starting metrics server.
 	if len(*metricsAddr) > 0 {
 		l, err := net.Listen("tcp", *metricsAddr)
 		if err != nil {
 			logger.Fatal("failed to start metrics end point", zap.Error(err))
 		}
+		logger.Info("starting metrics end point", zap.Stringer("addr", l.Addr()))
 		defer l.Close()
 		go func() {
 			err := http.Serve(l, promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
-			logger.Fatal("metrics end point exited", zap.Error(err))
+			if err != nil && !closed.Load() {
+				logger.Fatal("metrics end point exited", zap.Error(err))
+			}
 		}()
 	}
 
@@ -90,7 +95,7 @@ func main() {
 			Net:        "udp",
 			Handler:    h,
 		}
-		if err := server.ActivateAndServe(); err != nil {
+		if err := server.ActivateAndServe(); err != nil && !closed.Load() {
 			logger.Fatal("udp server exited", zap.Error(err))
 		}
 	}()
@@ -100,7 +105,7 @@ func main() {
 			Net:      "tcp",
 			Handler:  h,
 		}
-		if err := server.ActivateAndServe(); err != nil {
+		if err := server.ActivateAndServe(); err != nil && !closed.Load() {
 			logger.Fatal("tcp server exited", zap.Error(err))
 		}
 	}()
@@ -108,109 +113,8 @@ func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	sig := <-sigChan
+	closed.Store(true)
 	logger.Info("exiting", zap.Stringer("signal", sig))
-}
-
-type d2ip struct {
-	domains      map[string]struct{}
-	queryCounter prometheus.Counter
-	errCounter   prometheus.Counter
-}
-
-func (d *d2ip) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
-	resp := d.resp(w, r)
-	_ = w.WriteMsg(resp)
-}
-
-func (d *d2ip) resp(w dns.ResponseWriter, q *dns.Msg) *dns.Msg {
-	d.queryCounter.Inc()
-
-	// Reject invalid queries.
-	if len(q.Question) != 1 || q.Question[0].Qtype != dns.ClassINET {
-		logger.Warn("invalid query", zap.Stringer("from", w.RemoteAddr()))
-		d.errCounter.Inc()
-		return reject(q, dns.RcodeRefused)
-	}
-
-	question := q.Question[0]
-	// Parse ip addr from prefix.
-	s, suffixOk := trimFqdn(question.Name, d.domains)
-	if !suffixOk {
-		logger.Warn("invalid domain", zap.String("qname", question.Name), zap.Stringer("from", w.RemoteAddr()))
-		d.errCounter.Inc()
-		return reject(q, dns.RcodeRefused)
-	}
-
-	if strings.ContainsRune(s, '-') {
-		s = strings.ReplaceAll(s, "-", ":")
-	}
-	addr, err := netip.ParseAddr(s)
-	if err != nil {
-		logger.Warn("invalid ip", zap.String("qname", question.Name), zap.Stringer("from", w.RemoteAddr()))
-		d.errCounter.Inc()
-		return reject(q, dns.RcodeNameError)
-	}
-
-	logger.Info(
-		"incoming query",
-		zap.String("qname", question.Name),
-		zap.Uint16("qtype", question.Qtype),
-		zap.Stringer("ip", addr),
-		zap.Stringer("from", w.RemoteAddr()),
-	)
-
-	r := new(dns.Msg)
-	r.SetReply(q)
-	r.Authoritative = true
-	switch question.Qtype {
-	case dns.TypeA:
-		if !addr.Is4() {
-			break
-		}
-		r.Answer = append(r.Answer, &dns.A{
-			Hdr: dns.RR_Header{
-				Name:   question.Name,
-				Rrtype: dns.TypeA,
-				Class:  dns.ClassINET,
-				Ttl:    3600 * 24,
-			},
-			A: addr.AsSlice(),
-		})
-	case dns.TypeAAAA:
-		if !addr.Is6() {
-			break
-		}
-		r.Answer = append(r.Answer, &dns.AAAA{
-			Hdr: dns.RR_Header{
-				Name:   question.Name,
-				Rrtype: dns.TypeAAAA,
-				Class:  dns.ClassINET,
-				Ttl:    3600 * 24,
-			},
-			AAAA: addr.AsSlice(),
-		})
-	}
-	return r
-}
-
-func trimFqdn(fqdn string, domains map[string]struct{}) (string, bool) {
-	var suffixOk bool
-	for off, end := 0, false; !end; off, end = dns.NextLabel(fqdn, off) {
-		suffix := fqdn[off:]
-		if _, suffixOk = domains[suffix]; suffixOk {
-			if off > 0 {
-				return fqdn[:off-1], true
-			}
-			return "", true
-		}
-	}
-	return "", false
-}
-
-func reject(q *dns.Msg, rcode int) *dns.Msg {
-	r := new(dns.Msg)
-	r.SetRcode(q, rcode)
-	return r
 }
 
 func mustInitLogger() *zap.Logger {
